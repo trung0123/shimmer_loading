9e8c4c02645cf2b5e0698a6facfdcb8a1bb98a90
nesv005 <nesv005@gmail.com>
2023-03-20 16:29:32 +0700

    Finish shimmer

--- lib/main.dart
+++ lib/main.dart
@@ -48,10 +48,10 @@ class _ExampleUiLoadingAnimationState extends State<ExampleUiLoadingAnimation> {
           children: [
             const SizedBox(height: 16),
             _buildTopRowList(),
-            // const SizedBox(height: 16),
-            // _buildListItem(),
-            // _buildListItem(),
-            // _buildListItem()
+            const SizedBox(height: 16),
+            _buildListItem(),
+            _buildListItem(),
+            _buildListItem()
           ],
         ),
       ),
@@ -86,10 +86,10 @@ class _ExampleUiLoadingAnimationState extends State<ExampleUiLoadingAnimation> {
     return ShimmerLoading(isLoading: _isLoading, child: const CircleListItem());
   }
 
-  // Widget _buildListItem() {
-  //   return ShimmerLoading(
-  //       isLoading: _isLoading, child: CardListItem(isLoading: _isLoading));
-  // }
+  Widget _buildListItem() {
+    return ShimmerLoading(
+        isLoading: _isLoading, child: CardListItem(isLoading: _isLoading));
+  }
 }
 
 class Shimmer extends StatefulWidget {
@@ -264,72 +264,72 @@ class CircleListItem extends StatelessWidget {
   }
 }
 
-// class CardListItem extends StatelessWidget {
-//   const CardListItem({super.key, required this.isLoading});
-//
-//   final bool isLoading;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           _buildImage(),
-//           const SizedBox(height: 16),
-//           _buildText(),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   Widget _buildImage() {
-//     return AspectRatio(
-//       aspectRatio: 16 / 9,
-//       child: Container(
-//         width: double.infinity,
-//         decoration: BoxDecoration(
-//             color: Colors.black, borderRadius: BorderRadius.circular(16)),
-//         child: ClipRRect(
-//           borderRadius: BorderRadius.circular(16),
-//           child: Image.network(
-//             'https://flutter'
-//             '.dev/docs/cookbook/img-files/effects/split-check/Food1.jpg',
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildText() {
-//     if (isLoading) {
-//       return Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Container(
-//             width: double.infinity,
-//             height: 24,
-//             decoration: BoxDecoration(
-//                 color: Colors.black, borderRadius: BorderRadius.circular(16)),
-//           ),
-//           const SizedBox(height: 16),
-//           Container(
-//             width: 250,
-//             height: 24,
-//             decoration: BoxDecoration(
-//                 color: Colors.black, borderRadius: BorderRadius.circular(16)),
-//           ),
-//         ],
-//       );
-//     } else {
-//       return const Padding(
-//         padding: EdgeInsets.symmetric(horizontal: 8.0),
-//         child: Text(
-//             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do '
-//             'eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
-//       );
-//     }
-//   }
-// }
+class CardListItem extends StatelessWidget {
+  const CardListItem({super.key, required this.isLoading});
+
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildImage(),
+          const SizedBox(height: 16),
+          _buildText(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.black, borderRadius: BorderRadius.circular(16)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Image.network(
+            'https://flutter'
+            '.dev/docs/cookbook/img-files/effects/split-check/Food1.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    if (isLoading) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 24,
+            decoration: BoxDecoration(
+                color: Colors.black, borderRadius: BorderRadius.circular(16)),
+          ),
+          const SizedBox(height: 16),
+          Container(
+            width: 250,
+            height: 24,
+            decoration: BoxDecoration(
+                color: Colors.black, borderRadius: BorderRadius.circular(16)),
+          ),
+        ],
+      );
+    } else {
+      return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do '
+            'eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+      );
+    }
+  }
+}
